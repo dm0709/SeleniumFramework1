67f646202dd4319fe9500c8b05ec93a01e9c3353
dm0709 <dm070975@gmail.com>
2023-08-08 17:16:40 -0500

    bug fixing

--- src/test/java/TestCases/FirstTest.java
+++ src/test/java/TestCases/FirstTest.java
@@ -7,11 +7,11 @@ public class FirstTest {
 
     @Test
     public void StartDriver(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\dm070\\OneDrive\\Desktop\\TestProject1\\src\\test\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\dm070\\OneDrive\\Desktop\\SeleniumFramework1\\target\\test-classes\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         ChromeDriver driver = new ChromeDriver(options);
         driver.get("https://mvnrepository.com/open-source/logging-frameworks");
-        driver.get("https://google.com");
+
     }
 }
