35632b016587e9f6abb13b89425d54703471f1de
dm0709 <dm070975@gmail.com>
2023-08-08 16:38:02 -0500

    First commit

---
+++ src/test/java/TestCases/FirstTest.java
@@ -0,0 +1,17 @@
+package TestCases;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void StartDriver(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\dm070\\OneDrive\\Desktop\\TestProject1\\src\\test\\resources\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://mvnrepository.com/open-source/logging-frameworks");
+        driver.get("https://google.com");
+    }
+}
